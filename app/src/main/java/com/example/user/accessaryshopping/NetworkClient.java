4b0262db086a9bd4ab69c2654676cc18170738c4
201415059 _장민섭 <jms0995@hanmail.net>
2019-08-07 10:29:13 +0900

    Update NetworkClient.java

--- app/src/main/java/com/example/user/accessaryshopping/NetworkClient.java
+++ app/src/main/java/com/example/user/accessaryshopping/NetworkClient.java
@@ -9,7 +9,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 //레트로핏 쓸때 한군데에 클래스에서만 빌드 하기 위해서 안그러면 긴줄의 정보를 서버에 보내고 받을때 null 값이 뜬다.
 public class NetworkClient {
 
-    private static final String BASE_URL = "http://13.209.144.49/";
+    private static final String BASE_URL = "http:///";
     private static Retrofit retrofit;
 
     public static Retrofit getRetrofitClient(Context context) {
